c3b2b2bd87fb0c7f8b069016169197f891c612e2
Tulio Gerbassi <tuliogerbassi@gmail.com>
2023-08-08 22:40:53 +0800

    created components and views folders

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>sbb</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+</head>
+
+<body class="antialiased">
+    <nav class="border-b-4 ">
+        <ul class="flex p-4 gap-4 justify-center">
+            <li>
+                <a href="/">home</a>
+            </li>
+            <li>
+                <a href="/about">about</a>
+            </li>
+            <li>
+                <a href="/classes">classes</a>
+            </li>
+            <li>
+                <a href="/presentations">presentations
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <main class="h-screen">{{ $slot }}</main>
+    <footer class="border-t-4 text-center my-8 p-8 font-extrabold">FOOTER</footer>
+</body>
+
+</html>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+    <h1 class="text-5xl text-orange-600 text-center p-5">HOME</h1>
+</x-layout>
